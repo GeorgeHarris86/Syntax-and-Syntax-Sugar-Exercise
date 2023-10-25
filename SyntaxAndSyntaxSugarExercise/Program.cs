176fbc9467b2a05e2b7fc50f39af27782ba40a60
GeorgeHarris86 <geoharr21@gmail.com>
2023-10-25 09:27:34 -0400

    finished exercise

--- SyntaxAndSyntaxSugarExercise/Program.cs
+++ SyntaxAndSyntaxSugarExercise/Program.cs
@@ -1,10 +1,30 @@
-﻿namespace SyntaxAndSyntaxSugarExercise
+﻿using System.Net.NetworkInformation;
+
+namespace SyntaxAndSyntaxSugarExercise
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //+ inferred typing
+            //+ string interpolation
+            //+ and the ternary operator
+
+            //int answer = 4;
+            //string response;
+            //if (answer < 9)
+            //{
+            //    response = answer + " is less than nine";
+            //}
+            //else
+            //{
+            //    response = answer + " is greater than or equal to nine";
+            //}
+
+            var answer = 4;
+            var response = (answer < 9) ? $"{answer} is less than nine" : $"{answer} is Greater than or equal to nine";
+
+            Console.WriteLine(response);
         }
     }
 }
